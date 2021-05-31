a4a94d20a286d6508b247bc70718b5044ca9afde
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-31 10:18:46 -0300

    App.properties e TemaModel

---
+++ redeSocialG2/src/main/java/com/generation/redeSocialG2/model/TemaModel.java
@@ -0,0 +1,64 @@
+package com.generation.redeSocialG2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class TemaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5,max=100)
+	private String materia;
+	
+	@NotNull
+	@Size(min=2,max=50)
+	private String anoConteudo;
+	
+	@NotNull
+	@Size(min=10,max=1000)
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+	public String getAnoConteudo() {
+		return anoConteudo;
+	}
+
+	public void setAnoConteudo(String anoConteudo) {
+		this.anoConteudo = anoConteudo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
