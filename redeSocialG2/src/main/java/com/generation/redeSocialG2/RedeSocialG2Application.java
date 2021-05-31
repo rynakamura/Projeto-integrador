6c0cb4313d00c38f47c53f0c73db1e520490ceed
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-31 09:53:30 -0300

    Spring

---
+++ redeSocialG2/src/main/java/com/generation/redeSocialG2/RedeSocialG2Application.java
@@ -0,0 +1,13 @@
+package com.generation.redeSocialG2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialG2Application.class, args);
+	}
+
+}
---
+++ redeSocialG2/src/test/java/com/generation/redeSocialG2/RedeSocialG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.redeSocialG2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
