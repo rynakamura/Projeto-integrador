5281dc0549b6ad32220bd4921a790921af10b4c2
Andára <andarafinot@gmail.com>
2021-07-08 10:05:27 -0300

    Inserindo CRUD completo em UsuarioController

--- redeSocialG2/src/main/java/com/generation/redeSocialG2/controller/UsuarioController.java
+++ redeSocialG2/src/main/java/com/generation/redeSocialG2/controller/UsuarioController.java
@@ -1,18 +1,24 @@
 package com.generation.redeSocialG2.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.redeSocialG2.model.UserLoginModel;
 import com.generation.redeSocialG2.model.UsuarioModel;
+import com.generation.redeSocialG2.repository.UsuarioRepository;
 import com.generation.redeSocialG2.service.UsuarioService;
 
 @RestController
@@ -21,7 +27,20 @@ import com.generation.redeSocialG2.service.UsuarioService;
 public class UsuarioController 
 {
 	@Autowired
+	private UsuarioRepository repository;
+	
+	@Autowired
 	private UsuarioService usuarioService;
+	
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
 
 	@PostMapping ("/login")
 	public ResponseEntity<UserLoginModel> login (@RequestBody Optional<UserLoginModel> userLogin)
@@ -48,4 +67,13 @@ public class UsuarioController
 		}
 	}
 	
+	@PutMapping
+	public ResponseEntity<UsuarioModel> Put(@RequestBody UsuarioModel usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
